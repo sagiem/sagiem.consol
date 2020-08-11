bb4e9b5253da480add4446247358c423c3716962
sagiem <sagiems@gmail.com>
2020-08-11 15:44:38 +0600

    классы фигур

---
+++ урок 8/square.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace урок_8
+{
+    class square : figure
+    {
+    }
+}
