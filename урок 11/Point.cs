833208e324280bdcbf73afa832855dde41ecb57f
sagiem <sagiems@gmail.com>
2020-08-19 15:00:47 +0600

    operator reboot

---
+++ урок 11/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace урок_11
+{
+    class Point
+    {
+        public int x { get;}
+        public int y { get;}
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator==(Point p1, Point p2)
+        {
+            if(p1.x == p2.x|| p1.y == p2.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+         
+        }
+
+        public static bool operator !=(Point p1, Point p2)
+        {
+
+            return false;
+
+        }
+    }
+}
---
+++ урок 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace урок_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point1 = new Point(2, 5);
+            var point2 = new Point(7, 3);
+
+            var sumPoint = point1 == point2;
+
+            Console.WriteLine($"{sumPoint}");
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
