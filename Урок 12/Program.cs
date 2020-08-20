9c51812b560f6005f53e02da1aae7071fa20fe1f
sagiem <sagiems@gmail.com>
2020-08-20 16:56:34 +0600

    12

---
+++ Урок 12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
