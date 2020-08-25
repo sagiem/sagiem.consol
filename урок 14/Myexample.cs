f4e3fb9b6ab687e72e9193df97734882df4c9f00
sagiem <sagiems@gmail.com>
2020-08-25 15:29:35 +0600

    исключения

---
+++ урок 14/Myexample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace урок_14
+{
+    class Myexample : Exception
+
+    {
+        public Myexample():base("Это мое исключение")
+        {
+
+        }
+
+
+    }
+}
---
+++ урок 14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace урок_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            Console.WriteLine("Введите значение");
+            while(true)
+            {
+
+                if(int.TryParse(Console.ReadLine(), out result))
+
+                    {
+
+                    break;
+                    }
+                else
+                {
+                    Console.WriteLine("вы ввели неверное значение");
+                }
+                                   
+            }
+
+            try
+            {
+                //Console.WriteLine(10/result);
+
+                throw new Myexample();
+            }
+            catch(Myexample ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Упс...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
