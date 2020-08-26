edd5956a86fd772a1d5b6736aa902da1451f2b2a
sagiem <sagiems@gmail.com>
2020-08-26 17:46:55 +0600

    урок 16

---
+++ урок 16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace урок_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw= new StreamWriter("test.txt", true, Encoding.UTF8))
+            {
+                sw.Write("Привет ! ");
+                sw.WriteLine("Привет !");
+
+            }
+
+            using(var sr=new StreamReader("test.txt"))
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
