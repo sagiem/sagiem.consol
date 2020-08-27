53adac1846521b5ebd5148d663e68ae7535ddd07
sagiem <sagiems@gmail.com>
2020-08-27 10:58:53 +0600

    файлы

--- урок 16/Program.cs
+++ урок 16/Program.cs
@@ -8,10 +8,44 @@ namespace урок_16
     {
         static void Main(string[] args)
         {
+            string name;
+            int age;
+            string city;
+
+            Console.WriteLine("Введите ваше имя");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш возраст");
+            while(true)
+            {
+                if(int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result < 0 || result > 150)
+                    {
+                        Console.WriteLine("Вы ввели неверный возраст");
+                    }
+
+                    else
+                    {
+                        age = result;
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверный возраст");
+                }
+            }
+            Console.WriteLine("Введите город проживания");
+
+            city = Console.ReadLine();
+
+
+
             using (var sw= new StreamWriter("test.txt", true, Encoding.UTF8))
             {
-                sw.Write("Привет ! ");
-                sw.WriteLine("Привет !");
+                sw.WriteLine($"Привет ! Ваше имя {name} Ваш возраст {age} Вы из города {city}");
+                
 
             }
 
